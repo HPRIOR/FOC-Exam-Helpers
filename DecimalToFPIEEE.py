1129b8c8493226c49a1911e0161eeb82b76eab08
HPRIOR <hprior02@dcs.bbk.ac.uk>
2020-05-06 18:30:46 +0100

    decimal to ieee almost done

--- DecimalToFPIEEE.py
+++ DecimalToFPIEEE.py
@@ -25,7 +25,6 @@ def convert_post_dot_to_bin(f):
 
 
 def decimal_to_binary(number):
-    print("Converting the integer part of the number:")
     init_number = number
     div = 2
     binary_string = ""
@@ -34,6 +33,7 @@ def decimal_to_binary(number):
         binary_string = str(number % div) + binary_string
         number = number // div
     print(str(init_number) + ", is " + binary_string + " in binary")
+    print("")
     return binary_string
 
 
@@ -42,28 +42,68 @@ def convert_decimal_to_bin(number):
     return decimal_to_binary(float_splitter(number)[0]) + "." + convert_post_dot_to_bin(number)
 
 
-def calculate_exponent(s):
-    exp = len(s.split(".")[0]) - 1
-    print("exponent = " + str(exp))
-
-calculate_exponent("111.00")
-
-
 def exponent_string_manipulate(s):
     s = s.split(".")
     s = s[0] + s[1]
     return s[0] + "." + s[1:]
 
 
+def getmantisa(s):
+    return s.split(".")[1]
+
+
+def calculate_exponent(s):
+    exp = len(s.split(".")[0]) - 1
+    print("")
+    print("Find the exponent by moving up the decimal place: ")
+    print(s + " becomes: " + exponent_string_manipulate(s))
+    mantisa = getmantisa(exponent_string_manipulate(s))
+    print("Therefore:")
+    print("Mantisa = " + mantisa)
+    print(" ")
+    print("Exponent = " + str(exp))
+    print("Add the bias to the exponent: ")
+    exp_to_bin = exp + 127
+    print(str(exp) + " + 127 = " + str(exp_to_bin))
+    print("convert this to binary: ")
+    print("")
+    return decimal_to_binary(exp_to_bin), mantisa
+
+
 def calculate_sign(i):
     return 1 if i < 0 else 0
 
+def get_thirty_two_bit_mantisa(f):
+    for i in range(23-len(f)):
+        f = f + "0"
+    return f
+
+def split_into_groups_four(s):
+    string_return = ""
+    s = s[::-1]
+    for index, ch in enumerate(s):
+        if (index) % 4 == 0:
+            string_return += " " + ch
+        else:
+            string_return += ch
+    return string_return[::-1]
 
 def main():
-    fract_bin = convert_decimal_to_bin(17.625)
+    num = float(input("insert floating point number:"))
+    print("convert integer part of decimal to binary:")
+    print("")
+    fract_bin = convert_decimal_to_bin(num)
     print(" ")
     print("Binary representation of the fractional decimal: ")
     print(fract_bin)
+    exponent_mantisa = calculate_exponent(fract_bin)
+    sign = calculate_sign(num)
+    print("the sign bit is: " + str(sign))
+    print("the biased-exponent is: " + exponent_mantisa[0])
+    mantisa = get_thirty_two_bit_mantisa(exponent_mantisa[1])
+    print("the mantisa is: " + mantisa)
+    print("therefore the 32 bit floating point IEEE format is: ")
+    print(split_into_groups_four(str(sign) + exponent_mantisa[0] + mantisa))
 
 
 main()
